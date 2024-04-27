871a3df2172eb8dfcbf43895c1d879437ed21a39
Brecher <Brecher@students.uni-marburg.de>
2024-04-27 22:17:15 +0200

    created class Location and changed Kitchen

--- src/main/java/model/IIdentNumber.java
+++ src/main/java/model/IIdentNumber.java
@@ -1,2 +1,5 @@
-package model;public interface IIdentNumber {
+package model;
+
+public interface IIdentNumber {
+	//TODO: this
 }
--- src/main/java/model/Kitchen.java
+++ src/main/java/model/Kitchen.java
@@ -1,8 +1,12 @@
 package model;
 
-public record Kitchen(double longitude, double latitude, int story) {
+public record Kitchen(Location location, int story) {
+    public Kitchen(double kitchenLongitude, double kitchenLatitude, int story) {
+        this(new Location(kitchenLongitude, kitchenLatitude), story);
+    }
+
     @Override
     public String toString() {
-        return "Longitude: " + longitude + ", Latitude: " + latitude + ", Story: " + story;
+        return "Longitude: " + location.longitude() + ", Latitude: " + location.latitude() + ", Story: " + story;
     }
 }
