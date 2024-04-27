9634838a12d06e54312bfce41c3744a7fced4df8
Brecher <Brecher@students.uni-marburg.de>
2024-04-27 22:11:49 +0200

    added enum Course and interface IIdentNumber to prepare for future commit

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -5,6 +5,5 @@ import model.person.AgeRange;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println(AgeRange.getAgeRange(56).getAgeDifference(AgeRange.getAgeRange(57)));
 	}
 }
