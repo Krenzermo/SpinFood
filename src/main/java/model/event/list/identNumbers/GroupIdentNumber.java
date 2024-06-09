afdbebe12887db3cd97a0afe8e60828d96491a8e
piacenza <piacenza@students.uni-marburg.de>
2024-06-09 14:45:42 +0200

    GroupList refractored und GroupIdentNumber erstellt

--- src/main/java/model/event/list/identNumbers/GroupIdentNumber.java
+++ src/main/java/model/event/list/identNumbers/GroupIdentNumber.java
@@ -1,13 +1,16 @@
 package model.event.list.identNumbers;
 
+import model.event.collection.Group;
+import model.event.collection.Pair;
+import model.event.list.GroupList;
 import model.event.list.ParticipantCollectionList;
 
-public class GroupIdentNumber extends IdentNumber{
+import java.util.Arrays;
+
+public class GroupIdentNumber extends IdentNumber {
     private double averagePathLength;
 
-    //todo
-
-    protected GroupIdentNumber(ParticipantCollectionList participantCollection) {
+    public GroupIdentNumber(GroupList participantCollection) {
         super(participantCollection);
         genderDiversity = calcGenderDiversity(participantCollection);
         ageDifference = calcAgeDifference(participantCollection);
@@ -17,20 +20,47 @@ public class GroupIdentNumber extends IdentNumber{
 
     @Override
     protected double calcGenderDiversity(ParticipantCollectionList participantCollection) {
-        return 0;
+        GroupList groupList = (GroupList) participantCollection;
+        return groupList.getGroups().stream()
+                .flatMapToDouble(group -> Arrays.stream(group.getPairs()).mapToDouble(Pair::getGenderDeviation))
+                .average().orElse(0.0);
     }
 
     @Override
     protected double calcAgeDifference(ParticipantCollectionList participantCollection) {
-        return 0;
+        GroupList groupList = (GroupList) participantCollection;
+        return groupList.getGroups().stream()
+                .flatMapToDouble(group -> Arrays.stream(group.getPairs()).mapToDouble(Pair::getAgeDifference))
+                .average().orElse(0.0);
     }
 
     @Override
     protected double calcPreferenceDeviation(ParticipantCollectionList participantCollection) {
-        return 0;
+        GroupList groupList = (GroupList) participantCollection;
+        return groupList.getGroups().stream()
+                .flatMapToDouble(group -> Arrays.stream(group.getPairs()).mapToDouble(Pair::getPreferenceDeviation))
+                .average().orElse(0.0);
     }
 
     protected double calcAveragePathLength(ParticipantCollectionList participantCollection) {
-        return 0;
+        GroupList groupList = (GroupList) participantCollection;
+        return groupList.getGroups().stream()
+                .flatMapToDouble(group -> {
+                    Pair[] pairs = group.getPairs();
+                    double starterToMain = pairs[0].getKitchen().location().getDistance(pairs[1].getKitchen().location());
+                    double mainToDessert = pairs[1].getKitchen().location().getDistance(pairs[2].getKitchen().location());
+                    return Arrays.stream(new double[]{starterToMain, mainToDessert});
+                })
+                .average().orElse(0.0);
+    }
+
+    @Override
+    public String toString() {
+        return "Anzahl: " + numElems +
+                " Nachrücker: " + numSuccessors +
+                " Geschlechterdiversität: " + genderDiversity +
+                " Altersunterschied: " + ageDifference +
+                " Vorliebenabweichung: " + preferenceDeviation +
+                " Durchschnittliche Pfadlänge: " + averagePathLength;
     }
 }
