904e6cd928a14b051cf2c41c15fb4d7c341a2219
Daniel Hinkelmann <d.hinkelmann03@outlook.de>
2024-06-18 22:38:09 +0200

    created window for pairingweights setup

--- src/main/java/controller/FXMLControllers/MainController.java
+++ src/main/java/controller/FXMLControllers/MainController.java
@@ -1,11 +1,23 @@
 package controller.FXMLControllers;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.MenuItem;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 import model.event.io.InputData;
+import model.event.list.PairList;
+
+import java.io.File;
+import java.net.URL;
 
 public class MainController {
 
@@ -24,6 +36,27 @@ public class MainController {
 
     @FXML
     private MenuItem createPairs;
+
+    @FXML
+    private ListView<?> pairIdentNumbersList;
+
+    @FXML
+    private TableView<?> pairTable;
+
+    @FXML
+    private TableColumn<?, ?> partOneColPair;
+
+    @FXML
+    private TableColumn<?, ?> partTwoColPair;
+
+    @FXML
+    private ListView<?> successorListPair;
+
+    @FXML
+    private TableColumn<?, ?> kitchenColPair;
+
+    @FXML
+    private TableColumn<?, ?> courseColPair;
 
     @FXML
     void openFileChooserPartList(ActionEvent event) {
@@ -52,7 +85,18 @@ public class MainController {
     }
 
     @FXML
-    void executePairAlgorithm(ActionEvent event) {
+    void executePairAlgorithm(ActionEvent event) throws Exception{
+
+        String relPath = "src/main/java/view/fxml/pairingWeights.fxml";
+        File file = new File(relPath);
+        String absPath = file.getAbsolutePath();
+
+        URL url = new URL("file:///" + absPath);
+        Parent root = FXMLLoader.load(url);
+        Stage stage = new Stage();
+        stage.setTitle("Paar Parameter einstellen");
+        stage.setScene(new Scene(root, 369, 232));
+        stage.show();
 
     }
 
---
+++ src/main/java/controller/FXMLControllers/PairingWeightsController.java
@@ -0,0 +1,27 @@
+package controller.FXMLControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+
+public class PairingWeightsController {
+
+    @FXML
+    Button acceptPairingWeightsButton;
+
+    @FXML
+    private Slider ageDiffWeightSliderPair;
+
+    @FXML
+    private Slider fooPrefWeightSliderPair;
+
+    @FXML
+    private Slider genDivWeightSliderPair;
+
+    @FXML
+    void pairingWeiightsAccetpted(ActionEvent event) {
+
+    }
+
+}
